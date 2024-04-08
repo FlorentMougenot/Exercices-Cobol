65e7d3e5d5893c9af81bf6c992b4e1c835ca7792
FlorentMougenot <florent.mougenot@hotmail.fr>
2024-04-08 15:38:53 +0200

    add modifications

--- IntroCbl-Descriptions2/usertabl.cbl
+++ IntroCbl-Descriptions2/usertabl.cbl
@@ -4,9 +4,13 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 USERXX.
-        05 USER-ID         PIC X(2)9(3).
-        05 USER-CITY       PIC X(8).
-        05 USER-STREET     PIC 9(3)X(16).
-        05 USER-GSM-NBR    PIC 9(14).
-        05 USER-LAST-NAME  PIC X(9).
-        05 USER-FIRST-NAME PIC X(7).
+        05 USER-ID.
+         07 USER-ID-A        PIC A(2).
+         07 USER-ID-B        PIC 9(3).
+        05 USER-CITY         PIC X(8).
+        05 USER-STREET.
+         07 USER-STREET-A    PIC 9(3).
+         07 USER-STREET-B    PIC X(16).
+        05 USER-GSM-NBR      PIC 9(14).
+        05 USER-LAST-NAME    PIC X(9).
+        05 USER-FIRST-NAME   PIC X(7).
