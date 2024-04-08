28720d024301b0b423c0893cb0a0b033a3f6d43f
FlorentMougenot <florent.mougenot@hotmail.fr>
2024-04-08 14:45:22 +0200

    init usertabl

--- IntroCbl-Descriptions2/usertabl.cbl
+++ IntroCbl-Descriptions2/usertabl.cbl
@@ -4,9 +4,9 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 USERXX.
-        05 USER-ID PIC X(2)9(3).
-        05 USER-CITY PIC X(8).
-        05 USER-STREET PIC 9(3)X(16).
-        05 USER-GSM-NBR PIC 9(14).
-        05 USER-LAST-NAME PIC X(9).
+        05 USER-ID         PIC X(2)9(3).
+        05 USER-CITY       PIC X(8).
+        05 USER-STREET     PIC 9(3)X(16).
+        05 USER-GSM-NBR    PIC 9(14).
+        05 USER-LAST-NAME  PIC X(9).
         05 USER-FIRST-NAME PIC X(7).
