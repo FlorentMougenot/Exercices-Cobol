dc80f7136bad08f372535271012961fc678515ef
FlorentMougenot <florent.mougenot@hotmail.fr>
2024-04-10 15:39:50 +0200

    init poketable

---
+++ Calculatrice/trytable.cbl
@@ -0,0 +1,33 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. trytable.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       01 DATABASE.   
+           03 FILLER PIC X(32) VALUE "0001BULBIZARRE   PLT/PSN   0045".
+           03 FILLER PIC X(32) VALUE "0002HERBIZARRE   PLT/PSN   0060".
+           03 FILLER PIC X(32) VALUE "0003FLORIZARRE   PLT/PSN   0080".
+           03 FILLER PIC X(32) VALUE "0004SALAMECHE    FEU       0065".
+           03 FILLER PIC X(32) VALUE "0005REPTINCEL    FEU       0080".
+           03 FILLER PIC X(32) VALUE "0006DRACAUFEU    FEU/VOL   0100".
+           03 FILLER PIC X(32) VALUE "0007CARAPUCE     EAU       0043".
+           03 FILLER PIC X(32) VALUE "0008CARABAFFE    EAU       0053".
+           03 FILLER PIC X(32) VALUE "0009TORTANK      EAU       0078".
+           03 FILLER PIC X(32) VALUE "0010CHENIPAN     INSECT    0045".
+
+       01 POKEBALL REDEFINES DATABASE.
+           03 POKE-TABLE OCCURS 10 TIMES.
+              05 POKEDEX      PIC 9(4).
+              05 POKENAME     PIC X(13).
+              05 POKETYPE     PIC X(11).
+              05 POKESPEED    PIC 9(4).
+
+       PROCEDURE DIVISION.
+
+      * MOVE DATABASE TO POKEBALL.
+           DISPLAY "Deuxième entrée du tableau :" SPACE POKE-TABLE(2).
+           DISPLAY "Cinquième entrée du tableau :" SPACE POKE-TABLE(5).
+           DISPLAY "Dixième entrée du tableau :" SPACE POKE-TABLE(10).
+
+           STOP RUN.
